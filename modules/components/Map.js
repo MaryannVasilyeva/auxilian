7193d6d8bb710a562e649452213e239a690ff7c0
tashajohnson <tasha.hubner@gmail.com>
2016-05-12 18:06:14 -0600

    hard coded marker

--- modules/components/Map.js
+++ modules/components/Map.js
@@ -10,13 +10,54 @@ class Map extends React.Component {
 
   componentDidMount() {
     let mapboxgl = window.mapboxgl
-    mapboxgl.accessToken = 'pk.eyJ1IjoibXZhc2lseWV2YSIsImEiOiJjaW51dnV3eDUxMm5jdWdseXR3dW1wazRoIn0.l7HVScO-ipfCEwOmvDuGsg'
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGh1YmVzIiwiYSI6ImNpbnV5bGczdjEyczF1YW0zeTVreTJudWIifQ.GeJpORzICTOTDUEcnY9nPw'
+    
     let map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v8',
       center: [ -111.950684, 39.419220 ],
       zoom: 6
-    })   
+    }) 
+
+    map.on('load', function () {
+        map.addSource("markers", {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [-111.89, 40.76]
+                    },
+                    "properties": {
+                        "title": "You are here",
+                        "marker-symbol": "marker"
+                    }
+                }]
+            }
+        })
+
+        map.addLayer({
+            "id": "markers",
+            "type": "symbol",
+            "source": "markers",
+            "layout": {
+                "icon-image": "{marker-symbol}-15",
+                "text-field": "{title}",
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top"
+            }
+        })
+    })
+
+    map.addControl(new mapboxgl.Navigation({position: 'top-left'}))
+
+    var tooltip = new mapboxgl.Popup({closeOnClick: true})
+      .setLngLat([-111.89, 40.76])
+      .setHTML('<h1>You are here</h1>')
+      .addTo(map);
   }
 
   render() {
